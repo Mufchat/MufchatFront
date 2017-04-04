942d47722022d98194b5673762baff61365eca90
mmprogr <mmprogr@gmail.com>
2017-04-04 21:08:04 +0200

    Init

---
+++ js/chat.js
@@ -0,0 +1,23 @@
+// dane testowe
+var username = 'macholl';
+
+var greeting = new Vue({
+    el: '#greeting',
+    data: {
+        nickname: username
+    }
+});
+
+//dane testowe
+var onlineUsersList = [
+    {nickname: 'macholl', avatar: 'https://api.adorable.io/avatars/285/macholl.png'},
+    {nickname: 'muflon', avatar: 'https://api.adorable.io/avatars/285/muflon.png'},
+    {nickname: 'matiko101', avatar: 'https://api.adorable.io/avatars/285/matiko101.png'}
+];
+
+var onlineUsers = new Vue({
+    el: '#online-users',
+    data: {
+        onlineUsers: onlineUsersList
+    },
+});
