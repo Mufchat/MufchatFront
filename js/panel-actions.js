2d46fc046712c7f2313b2b8db8d811da1d47c5c3
Muflano <krecik09@hotmail.com>
2017-04-04 23:03:50 +0200

    Dodane funkcjonalności do panelu

---
+++ js/panel-actions.js
@@ -0,0 +1,11 @@
+$('#theme-switcher').change(function() {
+    $('body').toggleClass("light");
+});
+
+$('#personalize-button').click(function() {
+    $('#personalize-content').toggleClass('hidden');
+});
+
+$('#send-file').bind("click" , function () {
+        $('#send-file-core').click();
+});
